296cb14144cb13782d881e2a47ad060e49ac11ee
Rukin Danil <rukin2018d@yandex.ru>
2022-04-30 23:55:59 +0700

    Главное окно

---
+++ MedicalDocument/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalDocument.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public MainWindowViewModel MainWindowModel => App.Services.GetRequiredService<MainWindowViewModel>();
+    }
+}
